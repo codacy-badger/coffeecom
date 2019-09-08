0e26cd5d2a76502361ab906335dedd86c5810b42
Lars O.R.J <larsjuv@gmail.com>
2019-09-08 16:57:48 +0100

    Implemented reading from CONFIG

--- src/main/java/solarplus/coffeecom/ConfigurationHandler.java
+++ src/main/java/solarplus/coffeecom/ConfigurationHandler.java
@@ -4,6 +4,15 @@
 package solarplus.coffeecom;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+import java.lang.NullPointerException;
+
+import java.util.ArrayList;
 
 public class ConfigurationHandler {
 
@@ -11,13 +20,54 @@ public class ConfigurationHandler {
 	
 	private final String CONFIG_PATH = "../../../CONFIG.ccom";
 
+	// Pointer to CONFIG-file
+	private File file;
+
+	// BufferedReader to read data from CONFIG-file
+	private BufferedReader reader;
+
+	// BufferedWriter to write data to CONFIG-file
+	private BufferedWriter writer;
+
+	public ConfigurationHandler() {
+		try {
+			// Setting up instance of File (does not actually create a file 'CONFIG.ccom')
+			file = new File(CONFIG_PATH);
+
+			// Creates CONFIG-file if and only if it does not exist.
+			// true if file already exist, false if not
+			boolean configExist = file.exists();
+
+			// Setting up file-reader
+			reader = new BufferedReader(new FileReader(file));
+
+			// Setting up file-writer
+			boolean append = true;
+			writer = new BufferedWriter(new FileWriter(file, append));
+		} catch(NullPointerException npe) {
+			npe.printStackTrace();
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+	}
+
 	/**
-	 * Checks if the CONFIG.ccom-file exist.
+	 * Retrieves all lines from the CONFIG-file.
 	 *
-	 * @return true if file exists, false if file does not exist.
+	 * @return A List<String> where each String is a line from the file
 	 */
-	public boolean configFileExists() {
-		return new File(CONFIG_PATH).exists();
+	public ArrayList<String> getAllLines() {
+		ArrayList<String> lines = new ArrayList<>();
+		
+		try {
+			String line;
+			while((line = reader.readLine()) != null)
+				lines.add(line);
+		} catch(IOException ioe) {
+			ioe.printStackTrace();
+		}
+
+		return lines;
 	}
 }
 
